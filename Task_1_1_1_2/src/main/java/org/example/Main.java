f1a4ffdf98015d7b3b5a01008a8d1f0a969a6c6a
Иван Шебанов <vanamokanov8@gmail.com>
2024-09-04 18:19:39 +0700

    ehm

--- Task_1_1_1_2/src/main/java/org/example/Main.java
+++ Task_1_1_1_2/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell world!");
+        System.out.println("Goodbye world!");
     }
 }
