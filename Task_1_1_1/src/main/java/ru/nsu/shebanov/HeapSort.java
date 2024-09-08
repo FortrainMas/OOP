fcf478802eff202233cc068693ffa373d8363b9f
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-09-08 22:03:59 +0700

    Finished task 1

--- Task_1_1_1/src/main/java/ru/nsu/shebanov/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/shebanov/HeapSort.java
@@ -4,11 +4,39 @@ package ru.nsu.shebanov;
  * Sample class to simulate 1.1 task functionality
  */
 public class HeapSort {
-    public static void printHelloWorld() {
-        System.out.println("Hello world!");
+    private static void heapify(int[] arr, int N, int i){
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < N && arr[l] > arr[largest]) largest = l;
+        if (r < N && arr[r] > arr[largest]) largest = r;
+
+        if (largest != i) {
+            int tmp = arr[largest];
+            arr[largest] = arr[i];
+            arr[i] = tmp;
+
+            heapify(arr, N, largest);
+        }
+
     }
 
-    public static void main(String[] args) {
-        printHelloWorld();
+    public static int[] sort(int[] arr){
+        int N = arr.length;
+
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        for (int i = N - 1; i > 0; i--) {
+            int tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
+
+            heapify(arr, i, 0);
+        }
+
+        return arr;
+
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/shebanov/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/shebanov/HeapSortTest.java
@@ -2,23 +2,67 @@ package ru.nsu.shebanov;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapSortTest {
-
     @Test
-    void checkMain() {
-        HeapSort.main(new String[] {});
-        assertTrue(true);
+    void checkSort(){
+        int [] arr = {2,3,1,4,53,3};
+        int [] expected = {1,2,3,3,4,53};
+        int [] actual = HeapSort.sort(arr);
+        assertArrayEquals(actual, expected);
     }
 
     @Test
-    void checkPrinting() {
-        HeapSort.printHelloWorld();
+    void checkEmpty(){
+        int [] arr = {};
+        int [] expected = {};
+        int [] actual = HeapSort.sort(arr);
+        assertArrayEquals(actual, expected);
     }
 
     @Test
-    void doINeedToCheckDefaultConstructor() {
-        final var sampleInstance = new HeapSort();
+    void checkOneElement(){
+        int [] arr = {2};
+        int [] expected = {2};
+        int [] actual = HeapSort.sort(arr);
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void checkNegative(){
+        int [] arr = {-3, 2, -3, 9, 10};
+        int [] expected = {-3, -3, 2, 9, 10};
+        int [] actual = HeapSort.sort(arr);
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void checkSorted(){
+        int [] arr = {1, 2, 3, 4, 5};
+        int [] expected = {1,2,3,4,5};
+        int [] actual = HeapSort.sort(arr);
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void checkLarge(){
+        Random random = new Random();
+        int[] arr = new int[10000000];
+        int[] expected = new int[10000000];
+
+        for (int i = 0; i < arr.length; i++) {
+            int val = random.nextInt();
+            arr[i] = val;
+            expected[i] = val;
+        }
+        Arrays.sort(expected);
+
+        int [] actual = HeapSort.sort(arr);
+
+        assertArrayEquals(actual, expected);
     }
 }
