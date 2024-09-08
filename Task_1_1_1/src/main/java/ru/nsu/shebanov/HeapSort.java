c9939bff15cac9975945995f6c7dc2b34a8fa966
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-09-08 22:34:18 +0700

    fixes

--- Task_1_1_1/src/main/java/ru/nsu/shebanov/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/shebanov/HeapSort.java
@@ -1,42 +1,37 @@
 package ru.nsu.shebanov;
 
-/**
- * Sample class to simulate 1.1 task functionality
- */
+/** Sample class to simulate 1.1 task functionality */
 public class HeapSort {
-    private static void heapify(int[] arr, int N, int i){
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
+  private static void heapify(int[] arr, int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
 
-        if (l < N && arr[l] > arr[largest]) largest = l;
-        if (r < N && arr[r] > arr[largest]) largest = r;
+    if (l < n && arr[l] > arr[largest]) largest = l;
+    if (r < n && arr[r] > arr[largest]) largest = r;
 
-        if (largest != i) {
-            int tmp = arr[largest];
-            arr[largest] = arr[i];
-            arr[i] = tmp;
+    if (largest != i) {
+      int tmp = arr[largest];
+      arr[largest] = arr[i];
+      arr[i] = tmp;
 
-            heapify(arr, N, largest);
-        }
+      heapify(arr, n, largest);
+    }
+  }
 
+  public static int[] sort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+      int tmp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = tmp;
+
+      heapify(arr, i, 0);
     }
 
-    public static int[] sort(int[] arr){
-        int N = arr.length;
-
-        for (int i = N / 2 - 1; i >= 0; i--)
-            heapify(arr, N, i);
-
-        for (int i = N - 1; i > 0; i--) {
-            int tmp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = tmp;
-
-            heapify(arr, i, 0);
-        }
-
-        return arr;
-
-    }
+    return arr;
+  }
 }
