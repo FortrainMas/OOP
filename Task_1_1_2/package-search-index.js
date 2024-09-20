a5b854485aa845fc433b735e9afa7a784fbd98b5
FortrainMas <FortrainMas@users.noreply.github.com>
2024-09-20 06:06:30 +0000

    Deploying to gh-pages from @ FortrainMas/OOP@94dccfb0ce0112fb746024e32f481f96ea3f5178 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.shebanov"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"},{"l":"ru.nsu.shebanov"}]
