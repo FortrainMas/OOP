1e31a2300214612f7661f5e553c153037d995845
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-09-21 16:01:39 +0700

    Finished lab5.

---
+++ Task_1_2_2/src/main/java/ru/nsu/shebanov/HashTable.java
@@ -0,0 +1,249 @@
+package ru.nsu.shebanov;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+
+public class HashTable<K, V> implements Iterable<Entry<K, V>> {
+
+    private int tableLength;
+    private ArrayList<ArrayList<Entry<K, V>>> table;
+    private int modCount = 0;
+
+    HashTable() {
+        tableLength = 1000;
+        table = new ArrayList<>();
+        for (int i = 0; i < tableLength; i++) {
+            table.add(new ArrayList<>());
+        }
+    }
+
+    HashTable(int initialSize) {
+        tableLength = initialSize;
+        table = new ArrayList<>();
+        for (int i = 0; i < tableLength; i++) {
+            table.add(new ArrayList<>());
+        }
+    }
+
+    private void resize() {
+        int newTableLength = tableLength * 2;
+        ArrayList<ArrayList<Entry<K, V>>> newTable = new ArrayList<>();
+        for (int i = 0; i < newTableLength; i++) {
+            newTable = new ArrayList<>();
+        }
+
+        for (int i = 0; i < tableLength; i++) {
+            for (Entry<K, V> pair : table.get(i)) {
+                int hashCode = pair.getKey().hashCode() % newTableLength;
+                newTable.get(hashCode).add(new SimpleEntry<>(pair.getKey(), pair.getValue()));
+            }
+        }
+
+        tableLength = newTableLength;
+        table = newTable;
+    }
+
+    public void put(K key, V value) {
+        modCount += 1;
+        int hashCode = key.hashCode() % tableLength;
+
+        ArrayList<Entry<K, V>> collisionList = table.get(hashCode);
+        for (Entry<K, V> pair : collisionList) {
+            K curKey = pair.getKey();
+            if (curKey.equals(key)) {
+                pair.setValue(value);
+            }
+        }
+
+        collisionList.add(new SimpleEntry<>(key, value));
+
+        int COLLISION_MAXIMUM = 100;
+        if (collisionList.size() >= COLLISION_MAXIMUM) {
+            resize();
+        }
+    }
+
+    public void remove(K key) {
+        modCount += 1;
+        int hashCode = key.hashCode() % tableLength;
+
+        int entryPosition = -1;
+        ArrayList<Entry<K, V>> collisionList = table.get(hashCode);
+        for (int i = 0; i < collisionList.size(); i++) {
+            if (collisionList.get(i).getKey().equals(key)) {
+                entryPosition = i;
+                break;
+            }
+        }
+
+        collisionList.remove(entryPosition);
+    }
+
+    public V get(K key) {
+        int hashCode = key.hashCode() % tableLength;
+
+        ArrayList<Entry<K, V>> collisionList = table.get(hashCode);
+
+        if (collisionList == null) {
+            throw new NoSuchElementException("Key not found");
+        }
+
+        for (Entry<K, V> entry : collisionList) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+
+        throw new NoSuchElementException("Key not found");
+    }
+
+    public boolean contains(K key) {
+        int hashCode = key.hashCode() % tableLength;
+
+        ArrayList<Entry<K, V>> collisionList = table.get(hashCode);
+        for (Entry<K, V> entry : collisionList) {
+            if (entry.getKey().equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void update(K key, V value) {
+        modCount += 1;
+        int hashCode = key.hashCode() % tableLength;
+
+        ArrayList<Entry<K, V>> collisionList = table.get(hashCode);
+        for (Entry<K, V> pair : collisionList) {
+            K curKey = pair.getKey();
+            if (curKey.equals(key)) {
+                pair.setValue(value);
+                return;
+            }
+        }
+
+        throw new NoSuchElementException("Key not found");
+    }
+
+    public ArrayList<K> keys() {
+        ArrayList<K> keys = new ArrayList<>();
+        for (ArrayList<Entry<K, V>> collisionList : table) {
+            for (Entry<K, V> pair : collisionList) {
+                keys.add(pair.getKey());
+            }
+        }
+
+        return keys;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new Iterator<>() {
+
+            private int tableIndex = 0;
+            private int collisionIndex = 0;
+
+            private final int previousMods = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (previousMods != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (table.get(tableIndex).size() > collisionIndex) {
+                    return true;
+                }
+                tableIndex += 1;
+                collisionIndex = 0;
+                while (tableIndex != tableLength
+                        && table.get(tableIndex).size() == collisionIndex) {
+                    tableIndex += 1;
+                }
+
+                return tableIndex != tableLength;
+            }
+
+            @Override
+            public Entry<K, V> next() {
+                if (previousMods != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (table.get(tableIndex).size() > collisionIndex) {
+                    collisionIndex += 1;
+                    return table.get(tableIndex).get(collisionIndex - 1);
+                }
+                tableIndex += 1;
+                collisionIndex = 0;
+                while (tableIndex != tableLength
+                        && table.get(tableIndex).size() == collisionIndex) {
+                    tableIndex += 1;
+                }
+
+                collisionIndex += 1;
+                if (tableIndex == tableLength) {
+                    throw new NoSuchElementException();
+                }
+
+                return table.get(tableIndex).get(0);
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof HashTable<?, ?>)) {
+            return false;
+        }
+
+        ArrayList<K> keysList1;
+        ArrayList<K> keysList2;
+        try {
+            keysList1 = keys();
+            keysList2 = ((HashTable<K, V>) obj).keys();
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (!(keysList1.equals(keysList2))) {
+            return false;
+        }
+
+        for (K key : keysList1) {
+            if (!(get(key).equals(((HashTable<K, V>) obj).get(key)))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder("{\n");
+
+        for (ArrayList<Entry<K, V>> collisionList : table) {
+            for (Entry<K, V> pair : collisionList) {
+                output.append("\t")
+                        .append(pair.getKey())
+                        .append(" : ")
+                        .append(pair.getValue())
+                        .append("\n");
+            }
+        }
+
+        output.append("}");
+
+        return output.toString();
+    }
+}
---
+++ Task_1_2_2/src/test/java/ru/nsu/shebanov/HashTableTest.java
@@ -0,0 +1,146 @@
+package ru.nsu.shebanov;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+class HashTableTest {
+
+    @Test
+    void basicOperationsTest() {
+        HashTable<String, Integer> ht = new HashTable<>();
+
+        ht.put("ten", 10);
+        ht.put("one", 1);
+        ht.put("two", 2);
+
+        assertTrue(ht.contains("one"));
+        assertTrue(ht.contains("two"));
+        assertTrue(ht.contains("ten"));
+        assertFalse(ht.contains("three"));
+
+        ht.remove("one");
+        assertFalse(ht.contains("one"));
+
+        assertEquals(ht.get("ten"), 10);
+    }
+
+    @Test
+    void checkResizing() {
+        HashTable<String, Integer> ht = new HashTable<>(1);
+
+        ht.put("ten", 10);
+        ht.put("one", 1);
+        ht.put("two", 2);
+
+        assertTrue(ht.contains("one"));
+        assertTrue(ht.contains("two"));
+        assertTrue(ht.contains("ten"));
+        assertFalse(ht.contains("three"));
+
+        ht.remove("one");
+        assertFalse(ht.contains("one"));
+
+        assertEquals(ht.get("ten"), 10);
+    }
+
+    @Test
+    void checkUpdate() {
+        HashTable<String, Integer> ht = new HashTable<>();
+
+        ht.put("ten", 11);
+        ht.update("ten", 10);
+
+        assertEquals(ht.get("ten"), 10);
+    }
+
+    @Test
+    void checkKeyNotFoundExceptions() {
+        HashTable<String, Integer> ht = new HashTable<>();
+
+        try {
+            ht.get("one");
+            fail();
+        } catch (NoSuchElementException e1) {
+            try {
+                ht.update("one", 1);
+                fail();
+            } catch (NoSuchElementException e2) {
+                assertTrue(true);
+            }
+        }
+    }
+
+    @Test
+    void checkEquality() {
+        HashTable<String, Integer> ht1 = new HashTable<>();
+        HashTable<String, Integer> ht2 = new HashTable<>();
+
+        ht1.put("one", 1);
+        ht1.put("two", 2);
+        ht1.put("three", 3);
+
+        ht2.put("one", 1);
+        ht2.put("two", 2);
+        ht2.put("three", 3);
+
+        assertEquals(ht1, ht2);
+
+        ht2.put("four", 4);
+        assertNotEquals(ht1, ht2);
+
+        HashTable<Integer, Integer> ht3 = new HashTable<>();
+        assertNotEquals(ht1, ht3);
+    }
+
+    @Test
+    void checkToString() {
+        HashTable<String, Integer> ht = new HashTable<>();
+
+        ht.put("ten", 10);
+        ht.put("one", 1);
+        ht.put("two", 2);
+        ht.put("four", 4);
+        ht.put("five", 6);
+        ht.update("five", 5);
+
+        String expected =
+                "{\n"
+                        + "\tfour : 4\n"
+                        + "\tone : 1\n"
+                        + "\ttwo : 2\n"
+                        + "\tfive : 5\n"
+                        + "\tten : 10\n"
+                        + "}";
+        String actual = ht.toString();
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void iteratorTest() {
+        HashTable<String, Integer> ht = new HashTable<>();
+
+        ht.put("ten", 10);
+        ht.put("one", 1);
+        ht.put("two", 2);
+        ht.put("four", 4);
+        ht.put("five", 6);
+        ht.update("five", 5);
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> pare : ht) {
+            sb.append(pare.getKey()).append(" : ").append(pare.getValue()).append("\n");
+        }
+
+        String expected = "four : 4\n" + "one : 1\n" + "two : 2\n" + "five : 5\n" + "ten : 10\n";
+        assertEquals(sb.toString(), expected);
+    }
+}
