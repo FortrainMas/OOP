1a2b082660cbdc12acc22a92adf203331610db5d
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-11-12 19:47:49 +0700

    Initial commit

--- Task_1_1_1/build.sh
+++
@@ -1,4 +0,0 @@
-javadoc -d build/docs/javadoc -sourcepath src/main/java -subpackages ru.nsu.shebanov
-
-javac src/main/java/ru/nsu/shebanov/Heapsort.java
-java -cp src/main/java ru.nsu.shebanov.Heapsort
