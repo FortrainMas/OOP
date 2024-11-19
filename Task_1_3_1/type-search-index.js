84c71cb777d8c7936904b2833e28be824446c490
FortrainMas <FortrainMas@users.noreply.github.com>
2024-11-19 19:31:27 +0000

    Deploying to gh-pages from @ FortrainMas/OOP@e15ab3f3f7999185860be1ee9b60bce83bdccc26 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shebanov.z","l":"ZFunction"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shebanov.z","l":"Zfunction"}]
