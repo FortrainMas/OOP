1324e17bb7cbaec12a3466ca2731bf648df64d0b
Ivan Shebanov <i.shebanov@g.nsu.ru>
2025-02-07 02:34:00 +0700

    More my lovely fixes

--- Task_2_1_1/src/test/java/ru/nsu/shebanov/primes/SingleThreadComputeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/shebanov/primes/SingleThreadComputeTest.java
@@ -1,13 +1,13 @@
 package ru.nsu.shebanov.primes;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SingleThreadComputeTest {
     private final SingleThreadCompute stc = new SingleThreadCompute();
