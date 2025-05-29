3a4530c335fb16fc070ad4b632723b5d4ac52733
Ivan Shebanov <i.shebanov@g.nsu.ru>
2025-05-30 05:33:54 +0700

    resources

--- Task_2_4_1/src/test/java/ru/nsu/shebanov/githubDSL/DslTest.java
+++ Task_2_4_1/src/test/java/ru/nsu/shebanov/githubDSL/DslTest.java
@@ -1,21 +1,19 @@
 package ru.nsu.shebanov.githubDSL;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DslTest {
     @Test
     void runMainWithDslFile() throws Exception {
-        Path relativePath = Paths.get("src/main/java/ru/nsu/shebanov/githubDSL/config.dsl");
-
-        Path absolutePath = relativePath.toAbsolutePath().normalize();
-
-        if (!Files.exists(absolutePath)) {
-            throw new RuntimeException("🔥 DSL config not found at: " + absolutePath);
+        URL resource = getClass().getClassLoader().getResource("config.dsl");
+        if (resource == null) {
+            throw new RuntimeException("DSL config not found in resources!");
         }
 
+        Path absolutePath = Paths.get(resource.toURI());
         String[] args = { absolutePath.toString() };
 
         Main.main(args);
