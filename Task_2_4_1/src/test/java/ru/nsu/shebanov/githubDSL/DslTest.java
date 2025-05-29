7e2c9f0da7837d6c22c447cb23ec4440b96b0403
Ivan Shebanov <i.shebanov@g.nsu.ru>
2025-05-29 21:11:25 +0700

    Path fix

--- Task_2_4_1/src/test/java/ru/nsu/shebanov/githubDSL/DslTest.java
+++ Task_2_4_1/src/test/java/ru/nsu/shebanov/githubDSL/DslTest.java
@@ -1,12 +1,18 @@
 package ru.nsu.shebanov.githubDSL;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DslTest {
     @Test
     void runMainWithDslFile() throws Exception {
-        String[] args = {
-                "D:\\Z\\Programming\\_Univeristy\\NSU\\OOP\\_OOP\\Task_2_4_1\\src\\main\\java\\ru\\nsu\\shebanov\\githubDSL\\config.dsl"
-        };
+        Path relativePath = Paths.get("src/main/java/ru/nsu/shebanov/githubDSL/config.dsl");
+
+        String absolutePath = relativePath.toAbsolutePath().normalize().toString();
+
+        String[] args = { absolutePath };
+
         Main.main(args);
     }
 }
